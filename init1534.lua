8d16ae80719f1b9a54981bcdf143629abddc3a04
Myperior <95068783+TjZero1425@users.noreply.github.com>
2025-02-16 14:55:50 +0100

    Update init1534.lua

--- init1534.lua
+++ init1534.lua
@@ -7,92 +7,6 @@ getgenv().consoleinput = function() end
 getgenv().consoleprint = function() end
 getgenv().consolesettitle = function() end
 getgenv().rconsolename = function() end
-
-getgenv().bit = {}
-
-for i, v in next, bit32 do
-bit[i] = v
-end
-
-bit.ror = bit.rrotate
-bit.rol = bit.lrotate
-bit.rrotate = nil
-bit.lrotate = nil
-
-bit.badd = function(a, b)
-   return a + b
-end
-
-bit.bsub = function(a, b)
-   return a - b
-end
-
-bit.bdiv = function(a, b)
-   return a / b
-end
-
-bit.bmul = function(a, b)
-   return a * b
-end
-
-bit.tobit = function(x)
-  x = x % (2^32)
-  if x >= 0x80000000 then x = x - (2^32) end
-  return x
-end
-
-bit.tohex = function(x, n)
- n = n or 8
- local up
- if n <= 0 then
-   if n == 0 then return '' end
-   up = true
-   n = - n
- end
- x = bit.band(x, 16^n-1)
- return ('%0'..n..(up and 'X' or 'x')):format(x)
-end
-
-bit.bswap = function(x)
- local a = bit.band(x, 0xff)
- x = bit.rshift(x, 8)
- local b = bit.band(x, 0xff)
- x = bit.rshift(x, 8)
- local c = bit.band(x, 0xff)
- x = bit.rshift(x, 8)
- local d = bit.band(x, 0xff)
- return bit.lshift(bit.lshift(bit.lshift(a, 8) + b, 8) + c, 8) + d
-end
-
-getgenv().setthreadidentity = function(identity: number): ()
-    --_setidentity(identity)
-    task.wait()
-end
-
-getgenv().setidentity = getgenv().setthreadidentity
-getgenv().setthreadcontext = getgenv().setthreadidentity
-
-do
-    local CoreGui = game:GetService('CoreGui')
-    local HttpService = game:GetService('HttpService')
-
-    local comm_channels = CoreGui:FindFirstChild('comm_channels') or Instance.new('Folder', CoreGui)
-    if comm_channels.Name ~= 'comm_channels' then
-        comm_channels.Name = 'comm_channels'
-    end
-    getgenv().create_comm_channel = function() 
-        local id = HttpService:GenerateGUID()
-        local event = Instance.new('BindableEvent', comm_channels)
-        event.Name = id
-        return id, event
-    end
-
-    getgenv().get_comm_channel = function(id) 
-        assert(type(id) == 'string', 'string expected as argument #1')
-        return comm_channels:FindFirstChild(id)
-    end
-end
-
 getgenv().getactors = function()
     local actors = {};
     for i, v in game:GetDescendants() do
@@ -153,4 +67,24 @@ getgenv().hookmetamethod = function(obj, method, rep)
     
     return old
 end
---loadstring(game:HttpGet("https://raw.githubusercontent.com/TjZero1425/maindll/refs/heads/main/drawing1.lua"))()
+
+local oldreq = clonefunction(getrenv().require)
+getgenv().require = function(v)
+    local oldlevel = getthreadcontext()
+    local succ, res = pcall(oldreq, v)
+    if not succ and res:find('RobloxScript') then
+        succ = nil
+        coroutine.resume(coroutine.create(newcclosure(function()
+            setthreadcontext((oldlevel > 5 and 2) or 8)
+            succ, res = pcall(oldreq, v)
+        end)))
+        repeat task.wait() until succ ~= nil
+    end
+    
+    setthreadcontext(oldlevel)
+    
+    if succ then
+        return res
+    end
+end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TjZero1425/maindll/refs/heads/main/drawing1.lua"))()
