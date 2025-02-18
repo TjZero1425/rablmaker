b460102d0ff9ce3b85bace76f898fc626dbb434b
Myperior <95068783+TjZero1425@users.noreply.github.com>
2025-02-18 23:52:51 +0100

    Update init1535.lua

--- init1535.lua
+++ init1535.lua
@@ -106,6 +106,28 @@ getgenv().consoleprint = function() end
 getgenv().consolesettitle = function() end
 getgenv().rconsolename = function() end
 
+getgenv().syn_mouse1press=mouse1press
+getgenv().syn_mouse2click=mouse2click
+getgenv().syn_mousemoverel=movemouserel
+getgenv().syn_mouse2release=mouse2up
+getgenv().syn_mouse1release=mouse1up
+getgenv().syn_mouse2press=mouse2down
+getgenv().syn_mouse1click=mouse1click
+getgenv().syn_newcclosure=newcclosure
+getgenv().syn_clipboard_set=setclipboard
+getgenv().syn_clipboard_get=getclipboard
+getgenv().syn_islclosure=islclosure
+getgenv().syn_iscclosure=iscclosure
+getgenv().syn_getsenv=getsenv
+getgenv().syn_getscripts=getscripts
+getgenv().syn_getgenv=getgenv
+getgenv().syn_getinstances=getinstances
+getgenv().syn_getreg=getreg
+getgenv().syn_getrenv=getrenv
+getgenv().syn_getnilinstances=getnilinstances
+getgenv().syn_fireclickdetector=fireclickdetector
+getgenv().syn_getgc=getgc
+
 setreadonly(getgenv().debug,false)
 getgenv().debug.traceback = getrenv().debug.traceback
 getgenv().debug.profilebegin = getrenv().debug.profilebegin
