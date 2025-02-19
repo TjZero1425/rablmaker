337e167baa27e396ae2025e680d9541daf12d72d
Myperior <95068783+TjZero1425@users.noreply.github.com>
2025-02-19 15:32:10 +0100

    Update init1535.lua

--- init1535.lua
+++ init1535.lua
@@ -1,103 +1,4 @@
 if not game:IsLoaded() then game.Loaded:Wait() end
-
--- ADONIS ANTI CHEAT BYPASS
-pcall(function()
-local already_ran_adonis_bypass = false
-local Players = cloneref(game:GetService("Players"))
-local LocalPlayer = Players.LocalPlayer
-local PlayerGui = LocalPlayer:WaitForChild("PlayerGui") -- Infinite Yield until found
-
-local function runAdonisBypass()
-  if not getgenv().ADONIS_ANTI_CHEAT_BYPASS_Velocity then
-    getgenv().ADONIS_ANTI_CHEAT_BYPASS_Velocity = true
-    if not getgenv().ADONIS_DEBUG_INFO_BYPASS then
-      getgenv().ADONIS_DEBUG_INFO_BYPASS = true
-      local oldRBX_DebugInfo
-      oldRBX_DebugInfo = hookfunction(getrenv().debug.info, newcclosure(function(...)
-        local callingScript = getcallingscript()
-
-        if callingScript then
-          if tostring(callingScript) == "Client" and callingScript.Parent == nil then
-            task.wait(9e9)
-          end
-        end
-
-        return oldRBX_DebugInfo(...)
-      end))
-    end
-
-    for I, V in pairs(getgc(true)) do
-      if type(V) ~= "table" then continue end
-
-      if rawget(V, "Name") and rawget(V, "Running") and rawget(V, "Function") then
-        if V.Name == "AntiCoreGui" then
-          local oldCoreGuiFunc
-          oldCoreGuiFunc = hookfunction(V.Function, function(...)
-            return nil
-          end)
-        end
-      end
-    end
-
-    for I, V in pairs(getgc(true)) do
-      if type(V) ~= "table" then continue end
-
-      if rawget(V, "indexInstance") and rawget(V, "newindexInstance") then
-        if type(V.newindexInstance) == "table" then
-          for _I, _V in pairs(V) do
-            local badBoyFunc = V[_I][2]
-            if type(badBoyFunc) == "function" then
-              hookfunction(badBoyFunc, function()
-                return false
-              end)
-            end
-          end
-        end
-      end
-    end
-  end
-end
-
-local function findAdonisMenu()
-  if already_ran_adonis_bypass then return end
-  already_ran_adonis_bypass = true
-  for _, V in ipairs(PlayerGui:GetChildren()) do
-    if not V:IsA("ScreenGui") then continue end
-    
-    local Frame = V:FindFirstChild("Frame")
-    local Entry = V:FindFirstChild("Entry")
-    
-    local PlayerList = Frame and Frame:FindFirstChild("PlayerList")
-    local ScrollingFrame = Frame and Frame:FindFirstChild("ScrollingFrame")
-    local TopBar = Frame and Frame:FindFirstChild("TopBar")
-    local TextBox = Frame and Frame:FindFirstChild("TextBox")
-    
-    if Frame and Entry and PlayerList and ScrollingFrame and TopBar and TextBox then
-      runAdonisBypass()
-      warn("Adonis bypassed and clanked")
-      return true
-    end
-  end
-
-  for _, Instance in ipairs(getnilinstances()) do
-    if Instance:IsA("Folder") then
-      if Instance:FindFirstChild("Core") and Instance:FindFirstChild("Shared") and Instance:FindFirstChild("Dependencies") then
-        runAdonisBypass()
-        warn("Adonis bypassed and clanked 2")
-        return true
-      end
-    end
-  end
-
-  return false
-end
-
-findAdonisMenu()
-
-PlayerGui.ChildAdded:Connect(findAdonisMenu)
-end)
---
-
 getgenv().consoleclear = function() end
 getgenv().consolecreate = function() end
 getgenv().consoledestroy = function() end
@@ -105,28 +6,6 @@ getgenv().consoleinput = function() end
 getgenv().consoleprint = function() end
 getgenv().consolesettitle = function() end
 getgenv().rconsolename = function() end
-
-getgenv().syn_mouse1press=mouse1press
-getgenv().syn_mouse2click=mouse2click
-getgenv().syn_mousemoverel=movemouserel
-getgenv().syn_mouse2release=mouse2up
-getgenv().syn_mouse1release=mouse1up
-getgenv().syn_mouse2press=mouse2down
-getgenv().syn_mouse1click=mouse1click
-getgenv().syn_newcclosure=newcclosure
-getgenv().syn_clipboard_set=setclipboard
-getgenv().syn_clipboard_get=getclipboard
-getgenv().syn_islclosure=islclosure
-getgenv().syn_iscclosure=iscclosure
-getgenv().syn_getsenv=getsenv
-getgenv().syn_getscripts=getscripts
-getgenv().syn_getgenv=getgenv
-getgenv().syn_getinstances=getinstances
-getgenv().syn_getreg=getreg
-getgenv().syn_getrenv=getrenv
-getgenv().syn_getnilinstances=getnilinstances
-getgenv().syn_fireclickdetector=fireclickdetector
-getgenv().syn_getgc=getgc
 
 setreadonly(getgenv().debug,false)
 getgenv().debug.traceback = getrenv().debug.traceback
@@ -198,8 +77,6 @@ do
         return comm_channels:FindFirstChild(id)
     end)
 end
-
-
 
 local oldreq = clonefunction(getrenv().require)
 getgenv().require = newcclosure(function(v)
