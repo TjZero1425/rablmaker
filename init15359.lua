9bfbbb188dce9017227bcc6f6414aafe2b3cef8e
Myperior <95068783+TjZero1425@users.noreply.github.com>
2025-02-23 22:12:56 +0100

    Update init15359.lua

--- init15359.lua
+++ init15359.lua
@@ -77,4 +77,4 @@ getgenv().require = newcclosure(function(v)
     end
 end)
 
-loadstring(httpget("https://pastebin.com/raw/z3xBKccm"))()
+loadstring(httpget("https://raw.githubusercontent.com/TjZero1425/maindll/refs/heads/main/drawing1.lua"))()
