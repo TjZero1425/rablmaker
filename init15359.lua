ee708699c36b0623df4d8a7c318f21fd37e128fd
Myperior <95068783+TjZero1425@users.noreply.github.com>
2025-03-13 07:10:04 +0100

    Update init15359.lua

--- init15359.lua
+++ init15359.lua
@@ -15,9 +15,27 @@ getgenv().debug.getmetatable = getgenv().getrawmetatable
 getgenv().debug.setmetatable = getgenv().setrawmetatable
 getgenv().debug.info = getrenv().debug.info
 
-getgenv().isnetworkowner = function(part: BasePart): boolean
-    return part.ReceiveAge == 0 and not part.Anchored and part.Velocity.Magnitude > 0
-end
+local localPlayer = cloneref(game:GetService("Players").LocalPlayer);
+getgenv().isnetworkowner = newcclosure(function(part: BasePart): boolean
+    local root = localPlayer.Character and localPlayer.Character:FindFirstChild("HumanoidRootPart");
+    if root == nil or (root.Position - part.Position).Magnitude > gethiddenproperty(localPlayer, "SimulationRadius") then
+        return false;
+    end
+    if part:IsDescendantOf(localPlayer.Character) then
+        return true;
+    end
+    local player, mag = nil, math.huge;
+    for i, v in game:GetService("Players"):GetPlayers() do
+        local vRoot = v.Character and v.Character:FindFirstChild("HumanoidRootPart");
+        if vRoot then
+            local vMag = (vRoot.Position - part.Position).Magnitude;
+            if vMag < mag then
+                player, mag = v, vMag;
+            end
+        end
+    end
+    return player == localPlayer;
+end);
 
 getgenv().setsimulationradius = function(newRadius)
     assert(newRadius, `arg #1 is missing`)
@@ -39,6 +57,54 @@ getgenv().getsimulationradius = function()
         return LocalPlayer.SimulationRadius
     end
 end
+
+
+local bit = {
+    badd = newcclosure(function(a, b)
+        return a + b
+    end),
+    bsub = newcclosure(function(a, b)
+        return a - b
+    end),
+    bmul = newcclosure(function(a, b)
+        return a * b
+    end),
+    bdiv = newcclosure(function(a, b)
+        return bit32.rshift(a, b)
+    end),
+    tobit = newcclosure(function(a)
+        a = a % (2 ^ 32)
+        if a >= 0x80000000 then
+            a = a - (2 ^ 32)
+        end
+        return a
+    end),
+    bswap = newcclosure(function(a)
+        a = a % (2 ^ 32)
+        local b = bit32.band(a, 0xff)
+        a = bit32.rshift(a, 8)
+        local c = bit32.band(a, 0xff)
+        a = bit32.rshift(a, 8)
+        local d = bit32.band(a, 0xff)
+        a = bit32.rshift(a, 8)
+        return bit32.tobit(bit32.lshift(bit32.lshift(bit32.lshift(b, 8) + c, 8) + d, 8) + bit32.band(a, 0xff))
+    end),
+    ror = newcclosure(function(a, b)
+        return bit32.tobit(bit32.rrotate(a % 2 ^ 32, b % 32))
+    end),
+    rol = newcclosure(function(a, b)
+        return bit32.tobit(bit32.lrotate(a % 2 ^ 32, b % 32))
+    end),
+    tohex = newcclosure(function(num)
+        local hex = string.format("%08x", bit32.tobit(num))
+        return hex
+    end),
+}
+for i, v in bit32 do
+    bit[i] = v
+end
+getgenv().bit = bit
+setreadonly(bit, true);
 
 getgenv().getscriptfunction = getscriptclosure
 
