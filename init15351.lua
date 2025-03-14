919c51be0c7cde347bbcc23fbb74ff830d93efc8
Myperior <95068783+TjZero1425@users.noreply.github.com>
2025-03-14 07:31:43 +0100

    Update init15351.lua

--- init15351.lua
+++ init15351.lua
@@ -37,6 +37,44 @@ getgenv().isnetworkowner = newcclosure(function(part: BasePart): boolean
     return player == localPlayer;
 end);
 
+local ThreadLib = {}
+
+function ThreadLib.getscriptfromthread(thread)
+    return gettenv(thread).script
+end 
+
+local threads = {}
+function ThreadLib.getallthreads()
+    threads = {}
+    for i, v in next, getreg() do
+        if type(v) == "thread" then
+            table.insert(threads, v)
+        end
+    end
+    return threads
+end
+ThreadLib.getthreads = ThreadLib.getallthreads
+
+local connectedThreads = {}
+function ThreadLib.getscriptthreads(script)
+    connectedThreads = {}
+    for i, v in next, ThreadLib.getthreads() do
+        if ThreadLib.getscriptfromthread(v) == script then
+            table.insert(connectedThreads,v)
+        end
+    end
+    return connectedThreads
+end 
+
+function ThreadLib.getfunctionthreads(func)
+    local script = getfenv(func).script
+    return ThreadLib.getscriptthreads(script)
+end 
+
+for i, v in next, ThreadLib do
+    getfenv()[i] = v
+end
+
 local function register(i, v)
     getgenv()[i] = v
     return v
